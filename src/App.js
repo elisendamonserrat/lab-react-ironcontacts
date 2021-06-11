4dd5c9fdd22225c8ee4bc7f0da2f6fe123b7ecb5
Elisenda Monserrat <elisenda.monserrat@gmail.com>
2021-06-11 07:03:41 +0200

    does iteration 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import contacts from "./contacts.json";
 
 function App() {
+  console.log(contacts)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container w-4/5 py-8 mx-auto text-center">
+      <h1 className="text-3xl mb-8 font-bold">IronContacts</h1>
+      <table className="table-fixed w-2/5 mx-auto">
+        <tr>
+          <th className="w-1/4">Picture</th>
+          <th className="w-1/4">Name</th>
+          <th className="w-1/4">Popularity</th>
+        </tr>
+        {contacts.map((contact) => {
+          return (
+            <tr>
+              <td className="w-1/4">
+                <img className="w-1/3 inline mb-4" src={contact.pictureUrl} alt={contact.name} />
+              </td>
+              <td className="w-1/4">{contact.name}</td>
+              <td className="w-1/4">{contact.popularity.toFixed(2)}</td>
+            </tr>
+          );
+        }).slice(0, 5)
+        }
+      </table>
     </div>
   );
 }
